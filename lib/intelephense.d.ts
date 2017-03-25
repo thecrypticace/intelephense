8e5ac64b36e6fc0f735897d8e8fadabefdb4bf78
Ben Mewburn <ben@xps.mewburn>
2017-03-26 06:50:21 +1100

    renme isdocumentopen

--- lib/intelephense.d.ts
+++ lib/intelephense.d.ts
@@ -1,7 +1,7 @@
 import * as vscode from 'vscode-languageserver-types';
 export declare namespace Intelephense {
     function openDocument(uri: string, text: string): void;
-    function isDocumentOpen(uri: string): boolean;
+    function hasDocumentOpen(uri: string): boolean;
     function getDocument(uri: string): vscode.TextDocumentItem;
     function closeDocument(uri: string): void;
     function editDocument(uri: string, changes: vscode.TextDocumentContentChangeEvent[]): void;
