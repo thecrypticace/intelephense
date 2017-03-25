e094f7b0d1252e09cc87dcd9bbe5524458f53f7e
Ben Mewburn <ben@xps.mewburn>
2017-03-26 00:34:34 +1100

    getdocument

--- lib/intelephense.d.ts
+++ lib/intelephense.d.ts
@@ -1,8 +1,9 @@
-import * as lsp from 'vscode-languageserver-types';
+import * as vscode from 'vscode-languageserver-types';
 export declare namespace Intelephense {
     function openDocument(uri: string, text: string): void;
     function isDocumentOpen(uri: string): boolean;
+    function getDocument(uri: string): vscode.TextDocumentItem;
     function closeDocument(uri: string): void;
-    function editDocument(uri: string, changes: lsp.TextDocumentContentChangeEvent[]): void;
-    function documentSymbols(uri: string): lsp.SymbolInformation[];
+    function editDocument(uri: string, changes: vscode.TextDocumentContentChangeEvent[]): void;
+    function documentSymbols(uri: string): vscode.SymbolInformation[];
 }
