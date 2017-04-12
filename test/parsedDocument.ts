a741743106c7d1c0b7d11523a01bbf9d8d977db8
Ben Mewburn <ben@mewburn.id.au>
2017-04-12 23:26:53 +1000

    license, cleanup

---
+++ test/parsedDocument.ts
@@ -0,0 +1,22 @@
+import { ParsedDocument } from '../src/parsedDocument';
+import { assert } from 'chai';
+import 'mocha';
+
+var firstTokenSrc =
+`<?php
+    class MyClass { }
+`;
+
+describe('ParsedDocument' ,function(){
+
+    describe('firstToken', function(){
+
+        let doc = new ParsedDocument('test', firstTokenSrc);
+        let classNode = (<any>doc.tree).elements[1];
+        let tFirst = doc.firstToken(classNode);
+        console.log(JSON.stringify(doc.tree, ParsedDocument.stringyfyReplacer, 4));
+        console.log(JSON.stringify(tFirst, null, 4));
+
+    });
+
+});
