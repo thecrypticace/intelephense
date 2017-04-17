40aa8bed2ce794165bbb9a89ed29ea27efcd5b8a
Ben Mewburn <ben@mewburn.id.au>
2017-04-17 20:54:00 +1000

    sig help test

--- test/signatureHelpProvider.ts
+++ test/signatureHelpProvider.ts
@@ -1,25 +1,61 @@
 import { SignatureHelpProvider } from '../src/signatureHelpProvider';
 import { SymbolStore, SymbolTable, SymbolKind } from '../src/symbol';
 import { ParsedDocumentStore, ParsedDocument } from '../src/parsedDocument';
+import * as lsp from 'vscode-languageserver-types';
 import { assert } from 'chai';
 import 'mocha';
 
 
 let constructorHelpSrc =
 `<?php
-    class MyClass {
-        function __construct($param1, $param2){
-
-        }
+    class Foo {
+        function __construct($p1, $p2){}
     }
-    $var = new MyClass();
+    $var = new Foo()
 `;
 
-let inbuiltConstructorHelpSrc = 
+let functionHelpSrc = 
 `<?php
-    $pdo = new PDO()
+    function fn($p1, $p2){}
+    fn()
 `;
 
+let functionHelp2Src = 
+`<?php
+    function fn($p1, $p2){}
+    fn($x,)
+`;
+
+let methodHelpSrc = 
+`<?php
+    class Foo {
+        function bar($p1, $p2){}
+    }
+    $var = new Foo();
+    $var->bar()
+`;
+
+let defaultParamSrc = 
+`<?php
+    function fn($p1, $p2 = 1){}
+    fn($x,)
+`;
+
+let helpWithDocsSrc =
+`<?php
+    /**
+     * I'm a function
+     * @param int $p1 I'm a param
+     */
+    function fn($p1){}
+    fn()
+`;
+
+let helpWithTypeHintSrc = 
+`<?php
+    function fn(int $p1){}
+    fn()
+`;
 
 function setup(src: string) {
 
@@ -46,19 +82,185 @@ describe('SignatureHelpProvider', function () {
 
     describe('#provideSignatureHelp', function () {
 
-        it('constructor help', function () {
+        it('Constructor help', function () {
 
             let provider = setup(constructorHelpSrc);
-            let help = provider.provideSignatureHelp('test', {line: 6, character:23});
+            let help = provider.provideSignatureHelp('test', {line: 4, character:19});
+            let expected:lsp.SignatureHelp = {
+                activeParameter:0,
+                activeSignature:0,
+                signatures:[
+                    {
+                        documentation:undefined,
+                        label:'__construct($p1, $p2)',
+                        parameters:[
+                            {
+                                label:'$p1',
+                                documentation:undefined
+                            },
+                            {
+                                label:'$p2',
+                                documentation:undefined
+                            }
+                        ]
+                    }
+                ]
+            };
+            assert.deepEqual(help, expected);
             //console.log(JSON.stringify(help, null, 4)); 
 
         });
 
-        it('inbuilt symbol constructor help', function(){
-            let provider = setupInbuilt(inbuiltConstructorHelpSrc);
-            let help = provider.provideSignatureHelp('test', {line: 1, character:19});
-            console.log(JSON.stringify(help, null, 4)); 
+        it('Function help', function () {
+
+            let provider = setup(functionHelpSrc);
+            let help = provider.provideSignatureHelp('test', {line: 2, character:7});
+            let expected:lsp.SignatureHelp = {
+                activeParameter:0,
+                activeSignature:0,
+                signatures:[
+                    {
+                        label:'fn($p1, $p2)',
+                        documentation:undefined,
+                        parameters:[
+                            {
+                                label:'$p1',
+                                documentation:undefined
+                            },
+                            {
+                                label:'$p2',
+                                documentation:undefined
+                            }
+                        ]
+                    }
+                ]
+            };
+            assert.deepEqual(help, expected);
+            //console.log(JSON.stringify(help, null, 4)); 
+
         });
+
+        it('Function help second param', function () {
+
+            let provider = setup(functionHelp2Src);
+            let help = provider.provideSignatureHelp('test', {line: 2, character:10});
+            let expected:lsp.SignatureHelp = {
+                activeParameter:1,
+                activeSignature:0,
+                signatures:[
+                    {
+                        label:'fn($p1, $p2)',
+                        documentation:undefined,
+                        parameters:[
+                            {
+                                label:'$p1',
+                                documentation:undefined
+                            },
+                            {
+                                label:'$p2',
+                                documentation:undefined
+                            }
+                        ]
+                    }
+                ]
+            };
+            assert.deepEqual(help, expected);
+            //console.log(JSON.stringify(help, null, 4)); 
+
+        });
+
+        it('Method help', function () {
+
+            let provider = setup(methodHelpSrc);
+            let help = provider.provideSignatureHelp('test', {line: 5, character:14});
+            let expected:lsp.SignatureHelp = {
+                activeParameter:0,
+                activeSignature:0,
+                signatures:[
+                    {
+                        label:'bar($p1, $p2)',
+                        documentation:undefined,
+                        parameters:[
+                            {
+                                label:'$p1',
+                                documentation:undefined
+                            },
+                            {
+                                label:'$p2',
+                                documentation:undefined
+                            }
+                        ]
+                    }
+                ]
+            };
+            assert.deepEqual(help, expected);
+            //console.log(JSON.stringify(help, null, 4)); 
+
+        });
+
+        it('Function help default param sig 2', function () {
+
+            let provider = setup(defaultParamSrc);
+            let help = provider.provideSignatureHelp('test', {line: 2, character:10});
+            //console.log(JSON.stringify(help, null, 4));
+            assert.equal(help.signatures.length, 2);
+            assert.equal(help.signatures[0].label, 'fn($p1)');
+            assert.equal(help.signatures[1].label, 'fn($p1, $p2 = 1)');
+            assert.equal(help.activeParameter, 1);
+            assert.equal(help.activeSignature, 1);
+
+        });
+
+        it('Function help with docs', function () {
+
+            let provider = setup(helpWithDocsSrc);
+            let help = provider.provideSignatureHelp('test', {line: 6, character:7});
+            //console.log(JSON.stringify(help, null, 4));
+            let expected:lsp.SignatureHelp = {
+                activeParameter:0,
+                activeSignature:0,
+                signatures:[
+                    {
+                        documentation:"I'm a function",
+                        label:'fn(int $p1)',
+                        parameters:[
+                            {
+                                label:'int $p1',
+                                documentation:"I'm a param"
+                            }
+                        ]
+                    }
+                ]
+            };
+            assert.deepEqual(help, expected);
+
+        });
+
+        it('Function help with type hint', function () {
+
+            let provider = setup(helpWithTypeHintSrc);
+            let help = provider.provideSignatureHelp('test', {line: 2, character:7});
+            //console.log(JSON.stringify(help, null, 4));
+            let expected:lsp.SignatureHelp = {
+                activeParameter:0,
+                activeSignature:0,
+                signatures:[
+                    {
+                        documentation:undefined,
+                        label:'fn(int $p1)',
+                        parameters:[
+                            {
+                                label:'int $p1',
+                                documentation:undefined
+                            }
+                        ]
+                    }
+                ]
+            };
+            assert.deepEqual(help, expected);
+
+        });
+       
 
     });
 
