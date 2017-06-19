4547976ee2f8ddf3804cebf696e3a7060ce347da
Ben Mewburn <ben@mewburn.id.au>
2017-06-19 22:16:06 +1000

    importsymbol flushparsedebounce

--- src/intelephense.ts
+++ src/intelephense.ts
@@ -179,6 +179,7 @@ export namespace Intelephense {
     }
 
     export function importSymbol(textDocument:lsp.TextDocumentItem, position:lsp.Position) {
+        flushParseDebounce(textDocument.uri);
         return importSymbolCommand(symbolStore, documentStore, textDocument, position);
     }
 
