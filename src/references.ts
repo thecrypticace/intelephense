c84723d4e141d9d0a4d3731d8a50f64600309910
Ben Mewburn <ben@mewburn.id.au>
2017-07-17 00:30:14 +1000

    references WIP

--- src/references.ts
+++ src/references.ts
@@ -5,17 +5,17 @@
 'use strict';
 
 import { TreeVisitor, MultiVisitor } from './types';
-import {
-    Phrase, Token, PhraseType, TokenType,
-    QualifiedName
-} from 'php7parser';
+import { Phrase, Token, PhraseType, TokenType } from 'php7parser';
 import { SymbolKind, PhpSymbol } from './symbol';
 import { SymbolStore } from './symbolStore';
-import { ParsedDocument, NodeTransform } from './parsedDocument';
+import { ParsedDocument } from './parsedDocument';
+import {
+    NodeTransform, QualifiedNameTransform, RelativeQualifiedNameTransform,
+    FullyQualifiedNameTransform, DelimiteredListTransform
+} from './transforms';
 import { NameResolver } from './nameResolver';
 import { Predicate, BinarySearch, BinarySearchResult } from './types';
 import { NameResolverVisitor } from './nameResolverVisitor';
-import { VariableTypeVisitor, VariableTable } from './typeResolver';
 import { ParseTreeHelper } from './parseTreeHelper';
 import * as lsp from 'vscode-languageserver-types';
 import { isInRange } from './util';
@@ -52,6 +52,7 @@ export class ReferenceVisitor implements TreeVisitor<Phrase | Token> {
 
     private _references: Reference[];
     private _transformerStack: NodeTransform[];
+    private _variableTable:VariableTable;
 
     constructor(
         public doc: ParsedDocument,
@@ -60,10 +61,109 @@ export class ReferenceVisitor implements TreeVisitor<Phrase | Token> {
     ) {
         this._references = [];
         this._transformerStack = [];
+        this._variableTable = new VariableTable();
     }
 
     get references() {
         return new DocumentReferences(this.doc.uri, this._references);
+    }
+
+    preorder(node: Phrase | Token, spine: (Phrase | Token)[]) {
+
+        switch ((<Phrase>node).phraseType) {
+            case PhraseType.FunctionDeclaration:
+                this._methodOrFunction(node, SymbolKind.Function);
+                return true;
+            case PhraseType.MethodDeclaration:
+                this._methodOrFunction(node, SymbolKind.Method);
+                return true;
+            case PhraseType.ClassDeclaration:
+            case PhraseType.TraitDeclaration:
+            case PhraseType.InterfaceDeclaration:
+            case PhraseType.AnonymousClassDeclaration:
+                this._variableTable.pushScope();
+                return true;
+            case PhraseType.AnonymousFunctionCreationExpression:
+                this._anonymousFunctionCreationExpression(node);
+                return true;
+            case PhraseType.IfStatement:
+            case PhraseType.CaseStatement:
+            case PhraseType.DefaultStatement:
+            case PhraseType.ElseIfClause:
+                this._variableTable.pushBranch();
+                return true;
+            case PhraseType.ElseClause:
+                let elseClauseParent = spine[spine.length - 1];
+                if (!(<IfStatement>elseClauseParent).elseIfClauseList) {
+                    this._variableTable.popBranch();
+                }
+                this._variableTable.pushBranch();
+                return true;
+            case PhraseType.ElseIfClauseList:
+                this._variableTable.popBranch(); //pop the if branch
+                return true;
+            case PhraseType.SimpleAssignmentExpression:
+            case PhraseType.ByRefAssignmentExpression:
+                if (ParsedDocument.isPhrase((<BinaryExpression>node).left, [PhraseType.SimpleVariable, PhraseType.ListIntrinsic])) {
+                    this._assignmentExpression(<BinaryExpression>node);
+                    if (this.haltAtOffset > -1 && ParsedDocument.isOffsetInNode(this.haltAtOffset, node)) {
+                        this.haltTraverse = true;
+                    }
+                    return false;
+                }
+                return true;
+            case PhraseType.InstanceOfExpression:
+                this._instanceOfExpression(<InstanceOfExpression>node);
+                if (this.haltAtOffset > -1 && ParsedDocument.isOffsetInNode(this.haltAtOffset, node)) {
+                    this.haltTraverse = true;
+                }
+                return false;
+            case PhraseType.ForeachStatement:
+                this._foreachStatement(<ForeachStatement>node);
+                return true;
+            case PhraseType.CatchClause:
+                this._catchClause(<CatchClause>node);
+                return true;
+            case undefined:
+                this._token(<Token>node);
+                return false;
+            default:
+                return true;
+        }
+
+    }
+
+    postorder(node: Phrase | Token, spine: (Phrase | Token)[]) {
+
+        switch ((<Phrase>node).phraseType) {
+            case PhraseType.IfStatement:
+                if (!(<IfStatement>node).elseClause && !(<IfStatement>node).elseIfClauseList) {
+                    this._variableTable.popBranch();
+                }
+                this._variableTable.pruneBranches();
+                break;
+            case PhraseType.SwitchStatement:
+                this._variableTable.pruneBranches();
+                break;
+            case PhraseType.CaseStatement:
+            case PhraseType.DefaultStatement:
+            case PhraseType.ElseClause:
+            case PhraseType.ElseIfClause:
+                this._variableTable.popBranch();
+                break;
+            case PhraseType.FunctionDeclaration:
+            case PhraseType.MethodDeclaration:
+            case PhraseType.ClassDeclaration:
+            case PhraseType.TraitDeclaration:
+            case PhraseType.InterfaceDeclaration:
+            case PhraseType.AnonymousClassDeclaration:
+            case PhraseType.AnonymousFunctionCreationExpression:
+                this._variableTable.popScope();
+                break;
+            default:
+                break;
+        }
+
     }
 
     preorder(node: Phrase | Token, spine: (Phrase | Token)[]) {
@@ -143,115 +243,94 @@ export class ReferenceVisitor implements TreeVisitor<Phrase | Token> {
 
 }
 
-class FullyQualifiedNameTransformer implements NodeTransform {
+class MemberNameTransform implements NodeTransform {
 
-    protected _name: string;
-    protected _kindPredicate: Predicate<PhpSymbol>;
+    phraseType = PhraseType.MemberName;
+    value:Reference;
 
-    constructor(
-        public symbolStore: SymbolStore,
-        public range: lsp.Range,
-        public kind?: SymbolKind
-    ) {
-        switch (kind) {
-            case SymbolKind.Function:
-                this._kindPredicate = this.isFunction;
-                break;
-            case SymbolKind.Constant:
-                this._kindPredicate = this.isConstant;
-                break;
-            default:
-                this._kindPredicate = this.isTraitInterfaceClass;
-                break;
+    constructor(position:lsp.Position) {
+        this.value = Reference.create(PhpSymbol.create(SymbolKind.Method | SymbolKind.Property, ''), position, 0);
+    }
+
+    push(transform:NodeTransform) {
+        if(transform.tokenType === TokenType.Name) {
+            this.value.identifier.name = transform.value;
+            this.value.length = this.value.identifier.name.length;
         }
-    }
-
-    push(value: any, node: Phrase | Token) {
-        if ((<Phrase>node).phraseType === PhraseType.NamespaceName) {
-            this._name = value;
-        }
-    }
-
-    value() {
-
-        let name = this.fqn();
-
-        if (!name) {
-            return null;
-        }
-
-        let matches = this.symbolStore.match(this._name, this._kindPredicate);
-
-        if (matches.length > 0) {
-            return <Reference>{
-                range: this.range,
-                symbol: matches.length > 1 ? matches : matches.pop()
-            };
-        }
-
-        return null;
-    }
-
-    protected fqn() {
-        return this._name;
-    }
-
-    protected isFunction(x: PhpSymbol) {
-        return x.kind === SymbolKind.Function;
-    }
-
-    protected isConstant(x: PhpSymbol) {
-        return x.kind === SymbolKind.Constant;
-    }
-
-    protected isTraitInterfaceClass(x: PhpSymbol) {
-        return (x.kind & (SymbolKind.Class | SymbolKind.Trait | SymbolKind.Interface)) > 0;
     }
 
 }
 
-class RelativeQualifiedNameTransformer extends FullyQualifiedNameTransformer {
-    constructor(
-        public symbolStore: SymbolStore,
-        public range: lsp.Range,
-        public nameResolver: NameResolver,
-        public kind?: SymbolKind
-    ) {
-        super(symbolStore, range, kind);
+class ScopedMemberNameTransform implements NodeTransform {
+
+    phraseType = PhraseType.ScopedMemberName;
+    value:Reference;
+
+    constructor(position:lsp.Position) {
+        this.value = Reference.create(PhpSymbol.create(SymbolKind.Method | SymbolKind.Property | SymbolKind.ClassConstant, ''), position, 0);
     }
 
-    protected fqn() {
-        return this.nameResolver.resolveRelative(this._name);
+    push(transform:NodeTransform) {
+        if(transform.tokenType === TokenType.VariableName || transform.phraseType === PhraseType.Identifier) {
+            this.value.identifier.name = transform.value;
+            this.value.length = this.value.identifier.name.length;
+        }
     }
+
 }
 
-class QualifiedNameTransformer extends FullyQualifiedNameTransformer {
+class ScopedExpressionTransform implements NodeTransform {
 
-    constructor(
-        public symbolStore: SymbolStore,
-        public range: lsp.Range,
-        public nameResolver: NameResolver,
-        public kind?: SymbolKind
-    ) {
-        super(symbolStore, range, kind);
+    phraseType = PhraseType.ScopedCallExpression;
+    value:Reference;
+    private _symbolKind:SymbolKind;
+
+    constructor(symbolKind:SymbolKind) {
+        this._symbolKind = symbolKind;
     }
 
-    protected fqn() {
-        return this.nameResolver.resolveNotFullyQualified(this._name, this.kind);
+    push(transform:NodeTransform) {
+
+        if(transform.phraseType === PhraseType.ScopedMemberName) {
+            this.value = transform.value;
+            this.value.identifier.kind = this._symbolKind;
+        } else if(
+            transform.tokenType !== TokenType.ColonColon && 
+            transform.tokenType !== TokenType.Whitespace &&
+            transform.tokenType !== TokenType.Comment &&
+            transform.tokenType !== TokenType.DocumentComment
+        ) {
+            this.value.identifier.scope = transform.value;
+        }
     }
+
 }
+
+class S
+
 
 export interface Reference {
-    range: lsp.Range;
-    symbol: PhpSymbol | PhpSymbol[];
-    typeString?: string;
+    identifier:PhpSymbol;
+    position:lsp.Position;
+    length:number;
+    type?: string;
+}
+
+export namespace Reference {
+    export function create(identifier:PhpSymbol, position:lsp.Position, length:number){
+        return {
+            identifier: identifier,
+            position:position,
+            length:length
+        };
+    }
 }
 
 export class DocumentReferences {
 
     private _references: Reference[];
     private _uri: string;
-    private _search:BinarySearch<Reference>;
+    private _search: BinarySearch<Reference>;
 
     constructor(uri: string, references: Reference[]) {
         this._uri = uri;
@@ -271,9 +350,9 @@ export class DocumentReferences {
         return matches;
     }
 
-    referenceAtPosition(position:lsp.Position) {
+    referenceAtPosition(position: lsp.Position) {
 
-        let fn = (x:Reference) => {
+        let fn = (x: Reference) => {
             return isInRange(position, x.range.start, x.range.end);
         }
 
@@ -281,4 +360,144 @@ export class DocumentReferences {
 
     }
 
+}
+
+export class VariableTable {
+
+    private _typeVariableSetStack: TypedVariableSet[];
+
+    constructor() {
+
+        this._typeVariableSetStack = [{
+            kind: TypedVariableSetKind.Scope,
+            variables: {},
+            branches: []
+        }];
+    }
+
+    setType(varName: string, type: string) {
+        if (!varName || !type) {
+            return;
+        }
+        this._top().variables[varName] = { name: varName, type: type };
+    }
+
+    setTypeMany(varNames: string[], type: string) {
+        for (let n = 0, l = varNames.length; n < l; ++n) {
+            this.setType(varNames[n], type);
+        }
+    }
+
+    pushScope(carry?: string[]) {
+
+        let scope = <TypedVariableSet>{
+            kind: TypedVariableSetKind.Scope,
+            variables: {},
+            branches: []
+        }
+
+        if (carry) {
+            let type: string;
+            for (let n = 0; n < carry.length; ++n) {
+                type = this.getType(carry[n]);
+                if (type) {
+                    scope.variables[carry[n]] = { name: carry[n], type: type };
+                }
+            }
+        }
+
+        this._typeVariableSetStack.push(scope);
+
+    }
+
+    popScope() {
+        this._typeVariableSetStack.pop();
+    }
+
+    pushBranch() {
+        let b = <TypedVariableSet>{
+            kind: TypedVariableSetKind.Branch,
+            variables: {},
+            branches: []
+        };
+        this._top().branches.push(b);
+        this._typeVariableSetStack.push(b);
+    }
+
+    popBranch() {
+        this._typeVariableSetStack.pop();
+    }
+
+    /**
+     * consolidates variables. 
+     * each variable can be any of types discovered in branches after this.
+     */
+    pruneBranches() {
+
+        let node = this._top();
+        let branches = node.branches;
+        node.branches = [];
+        for (let n = 0, l = branches.length; n < l; ++n) {
+            this._mergeSets(node, branches[n]);
+        }
+
+    }
+
+    getType(varName: string, className?: string) {
+
+        if (varName === '$this' && className) {
+            return className;
+        }
+
+        let typeSet: TypedVariableSet;
+
+        for (let n = this._typeVariableSetStack.length - 1; n >= 0; --n) {
+            typeSet = this._typeVariableSetStack[n];
+            if (typeSet.variables[varName]) {
+                return typeSet.variables[varName].type;
+            }
+
+            if (typeSet.kind === TypedVariableSetKind.Scope) {
+                break;
+            }
+        }
+
+        return '';
+
+    }
+
+    private _mergeSets(a: TypedVariableSet, b: TypedVariableSet) {
+
+        let keys = Object.keys(b.variables);
+        let typedVar: TypedVariable;
+        for (let n = 0, l = keys.length; n < l; ++n) {
+            typedVar = b.variables[keys[n]];
+            if (a.variables[typedVar.name]) {
+                a.variables[typedVar.name].type = TypeString.merge(a.variables[typedVar.name].type, typedVar.type);
+            } else {
+                a.variables[typedVar.name] = typedVar;
+            }
+        }
+
+    }
+
+    private _top() {
+        return this._typeVariableSetStack[this._typeVariableSetStack.length - 1];
+    }
+
+}
+
+interface TypedVariable {
+    name: string;
+    type: string;
+}
+
+const enum TypedVariableSetKind {
+    None, Scope, BranchGroup, Branch
+}
+
+interface TypedVariableSet {
+    kind: TypedVariableSetKind;
+    variables: { [index: string]: TypedVariable };
+    branches: TypedVariableSet[];
 }
