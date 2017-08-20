ac7afcb32cf108c1e4e1786af581feb84a8611bb
Ben Mewburn <ben@mewburn.id.au>
2017-08-20 18:46:50 +1000

    tests

--- test/parsedDocument.ts
+++ test/parsedDocument.ts
@@ -3,19 +3,31 @@ import { assert } from 'chai';
 import 'mocha';
 
 var firstTokenSrc =
-`<?php
+    `<?php
     class MyClass { }
 `;
 
-describe('ParsedDocument' ,function(){
+describe('ParsedDocument', function () {
 
-    describe('firstToken', function(){
+    describe('firstToken', function () {
 
-        let doc = new ParsedDocument('test', firstTokenSrc);
-        let classNode = (<any>doc.tree).elements[1];
-        let tFirst = ParsedDocument.firstToken(classNode);
-        console.log(JSON.stringify(doc.tree, ParsedDocument.stringyfyReplacer, 4));
-        console.log(JSON.stringify(tFirst, null, 4));
+        it('returns first token of phrase', () => {
+
+            let doc = new ParsedDocument('test', firstTokenSrc);
+            let classNode = doc.tree.children[2];
+            let tFirst = ParsedDocument.firstToken(classNode);
+            let expected = {
+                tokenType: 9,
+                offset: 10,
+                length: 5,
+                modeStack: [
+                    1
+                ]
+            };
+            //console.log(JSON.stringify(tFirst, null, 4));
+            assert.deepEqual(tFirst, expected);
+
+        })
 
     });
 
