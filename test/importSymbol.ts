3c48b78df9e641e5cacfc25e3ee73126442a3e42
Ben Mewburn <ben@mewburn.id.au>
2017-10-27 22:44:30 +1100

    import symbol test refactor

--- test/importSymbol.ts
+++ test/importSymbol.ts
@@ -5,6 +5,8 @@ import { ParsedDocument, ParsedDocumentStore } from '../src/parsedDocument';
 import { SymbolStore, SymbolTable } from '../src/symbolStore';
 import { NameTextEditProvider } from '../src/commands';
 import {ReferenceReader} from '../src/referenceReader';
+import {ReferenceStore} from '../src/reference';
+import {MemoryCache} from '../src/cache';
 
 describe('importSymbol', () => {
 
@@ -23,6 +25,7 @@ describe('importSymbol', () => {
     let doc1 = new ParsedDocument('doc1', src1);
     let doc2 = new ParsedDocument('doc2', src2);
     let docStore = new ParsedDocumentStore();
+    let refStore = new ReferenceStore(new MemoryCache());
     docStore.add(doc1);
     docStore.add(doc2);
     let symbolStore = new SymbolStore();
@@ -30,10 +33,10 @@ describe('importSymbol', () => {
     let t2 = SymbolTable.create(doc2);
     symbolStore.add(t1);
     symbolStore.add(t2);
-    ReferenceReader.discoverReferences(doc1, t1, symbolStore);
-    ReferenceReader.discoverReferences(doc2, t2, symbolStore);
-    symbolStore.indexReferences(t1);
-    symbolStore.indexReferences(t2);
+    let refTable = ReferenceReader.discoverReferences(doc1, symbolStore);
+    let refTable2 = ReferenceReader.discoverReferences(doc2, symbolStore);
+    refStore.add(refTable);
+    refStore.add(refTable2);
 
     let expected = [
         {
@@ -67,7 +70,7 @@ describe('importSymbol', () => {
 
     it('Should return text edits when a symbol can be imported', () => {
 
-        let provider = new NameTextEditProvider(symbolStore, docStore);
+        let provider = new NameTextEditProvider(symbolStore, docStore, refStore);
         let edits = provider.provideContractFqnTextEdits('doc2', { line: 2, character: 27 });
         //console.log(JSON.stringify(edits, null, 4));
         assert.deepEqual(edits, expected);
